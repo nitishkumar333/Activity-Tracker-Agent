da4bb160a19cd78d2ae590004a0eb4eefc1da879
nitishkumar333 <ae.nitishkumar@gmail.com>
2024-10-02 15:00:52 +0530

    remove cache issue

--- activity-tracking/timezone_detection.py
+++ activity-tracking/timezone_detection.py
@@ -1,14 +1,18 @@
 import time
-from tzlocal import get_localzone
+import pytz
+from datetime import datetime
+from tzlocal import get_localzone, reload_localzone
 
 def detect_time_zone_change():
     previous_tz = get_localzone()
     print(previous_tz)
     while True:
+        reload_localzone()
         current_tz = get_localzone()
         print(current_tz)
         if current_tz != previous_tz:
             print("\nTime zone change detected!")
+            previous_tz = current_tz
 
         time.sleep(10)
 
