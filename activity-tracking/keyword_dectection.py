8463b0a4a7fd7dc144a34fe4c62dd37fa27b41d6
nitishkumar333 <ae.nitishkumar@gmail.com>
2024-10-02 12:41:08 +0530

    added comments

--- activity-tracking/keyword_dectection.py
+++ activity-tracking/keyword_dectection.py
@@ -1,9 +1,10 @@
 import keyboard
 import time
 
-THRESHOLD = 0.2
-BUFFER_SIZE = 100
+THRESHOLD = 0.2   # Maximum time between keypresses for automated/scripted input
+BUFFER_SIZE = 100         # Number of key presses to analyze
 
+# A buffer to store recent key press timings
 key_press_times = []
 curr_count = 0
 flag = 0
@@ -12,16 +13,20 @@ def on_key_event(e):
     global flag
     current_time = time.time()
     
+    # Only consider key down events
     if e.event_type == 'down':
         if key_press_times:
+            # Calculate the time difference between the last key press and current one
             time_diff = current_time - key_press_times[-1]
             
             if time_diff < THRESHOLD:
                 curr_count += 1
         
+        # Add current time to the buffer
         key_press_times.append(current_time)
         flag += 1
 
+        # Keep only the last 100 timings
         if len(key_press_times) >= BUFFER_SIZE:
             if curr_count >= 90 and flag >= 100:
                 print("Bot detected!")
@@ -30,6 +35,6 @@ def on_key_event(e):
                 curr_count -= 1
             key_press_times.pop(0)
 
+# Hook the key press event
 keyboard.hook(on_key_event)
 print("Monitoring keyboard input....")
-keyboard.wait('esc')
