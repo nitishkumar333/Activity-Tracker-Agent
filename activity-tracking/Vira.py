7e4d192863baaa4effc1820cd2d73145b231c67d
Adixit8604 <abhimrt8604@gmail.com>
2024-10-03 00:35:02 +0530

    saari galti sudhari

--- activity-tracking/Vira.py
+++ activity-tracking/Vira.py
@@ -8,6 +8,9 @@ from kivy.properties import StringProperty
 from kivy.core.window import Window
 from kivy.uix.textinput import TextInput
 from kivy.uix.checkbox import CheckBox
+from kivy.uix.popup import Popup
+from system_info import get_system_info
+from tracking_logic.final import Final_Tracker
 
 import pyautogui
 import threading
@@ -16,30 +19,69 @@ import os
 from PIL import Image, ImageFilter
 
 
+
 class HomeScreen(Screen):
-    timer_text = StringProperty("00:00:00")
+    timer_text = StringProperty("00:00:00")  
 
     def __init__(self, **kwargs):
         super(HomeScreen, self).__init__(**kwargs)
-        self.seconds = 0
+        self.seconds = 0  
         self.timer_event = None
+        self.bot_activity_detected=[False]
+        self.stop=[True]
+        self.stop_event = threading.Event() 
+        self.activity_thread = None
+        self.stop_thread()
 
     def start_timer(self):
-        if not self.timer_event:  # Only start if not already started
+        if not self.timer_event:  
             self.timer_event = Clock.schedule_interval(self.update_timer, 1)
+            self.stop[0]=False
+            self.start_thread()  
 
     def stop_timer(self):
         if self.timer_event:
             self.timer_event.cancel()
             self.timer_event = None
+        self.stop[0]=True
         self.seconds = 0  # Reset seconds
         self.timer_text = "00:00:00"  # Reset timer display
+        self.stop_thread()  # Stop the bot activity monitoring thread
 
     def update_timer(self, dt):
         self.seconds += 1
         hours, remainder = divmod(self.seconds, 3600)
         minutes, seconds = divmod(remainder, 60)
-        self.timer_text = f"{hours:02}:{minutes:02}:{seconds:02}"
+        self.timer_text = f"{hours:02}:{minutes:02}:{seconds:02}" 
+
+    
+    def start_thread(self):
+        self.stop_event.clear()  # Clear the stop event
+        self.activity_thread = threading.Thread(target=Final_Tracker, args=(self.bot_activity_detected,self.stop))
+        self.activity_Pop_thread = threading.Thread(target=self.monitor_bot_activity)
+        self.activity_thread.daemon = True  
+        self.activity_Pop_thread.daemon = True  
+        self.activity_thread.start()
+        self.activity_Pop_thread.start()
+
+    def stop_thread(self):
+        self.stop_event.set()  # Set the event to signal the thread to stop 
+        if self.activity_thread is not None:
+            self.activity_thread.join()
+
+    def monitor_bot_activity(self):
+        while not self.stop_event.is_set():  
+            if self.bot_activity_detected[0]:
+                Clock.schedule_once(self.show_bot_warning, 0)  # Show warning immediately
+                self.bot_activity_detected[0] = False  # Reset the flag after detection
+            threading.Event().wait(1)
+    
+    def show_bot_warning(self, dt):
+        popup = Popup(title='Warning',
+                      content=Label(text='Bot activity detected!'),
+                      size_hint=(0.6, 0.3))
+        popup.open()
+        Clock.schedule_once(lambda dt: popup.dismiss(), 5)  
 
 
 class ConfigScreen(Screen):
@@ -123,9 +165,10 @@ class AboutScreen(Screen):
 
 
 class ViraMain(BoxLayout):
+    system_info = get_system_info() 
     def __init__(self, **kwargs):
         super(ViraMain, self).__init__(**kwargs)
-        Window.set_icon('Images/Viraico.png')
+        Window.set_icon('Images/Viragfadsgfasico.png')
         Window.minimum_width = 550
         Window.minimum_height = 500
         Window.bind(on_resize=self.on_window_resize)
