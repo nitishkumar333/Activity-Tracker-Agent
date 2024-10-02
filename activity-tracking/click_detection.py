5f3753ea88db1c01fa7ba5066f8718ce74769884
nitishkumar333 <ae.nitishkumar@gmail.com>
2024-10-02 14:41:42 +0600

    on click function

--- activity-tracking/click_detection.py
+++ activity-tracking/click_detection.py
@@ -10,7 +10,22 @@ click_count = 0
 last_position = 0
 
 def on_click(x, y, button, pressed):
-    return
+    global click_count, last_position
+
+    if pressed:
+        if last_position is None:
+            last_position = (x, y)
+
+        current_position = (x, y)
+        if current_position == last_position:
+            click_count += 1
+        else:
+            click_count = 1
+            last_position = current_position
+
+        if click_count > CLICK_LIMIT:
+            print("Bot detection triggered (excessive clicks without movement)")
+            click_count = 0
 
 def on_move(x, y):
     global click_count, last_position
