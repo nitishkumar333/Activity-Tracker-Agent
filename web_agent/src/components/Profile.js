cdb043978d64c12a862b4997562383275525dcb8
nitishkumar333 <ae.nitishkumar@gmail.com>
2024-10-03 21:32:40 +0530

    config commponent

---
+++ web_agent/src/components/Profile.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Config from "./Config.js";
+
+function Profile() {
+  return (
+    <div className="outerbox">
+      <div className="innerbox">
+        <main className="signup-body">
+          <Config />
+        </main>
+      </div>
+    </div>
+  );
+}
+export default Profile;
