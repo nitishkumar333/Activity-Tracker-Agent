133e4f327ab007649d6fb43bf40c5ebd961fa57c
Adixit8604 <abhimrt8604@gmail.com>
2024-10-04 02:21:58 +0530

    Queue Complete

--- activity-tracking/tempCodeRunnerFile.py
+++ activity-tracking/tempCodeRunnerFile.py
@@ -1,7 +1,28 @@
-self.stop_event.set()
-        if self.activity_thread is not None:
-            self.activity_thread.join()
-            self.activity_thread = None  # Clear reference after stopping
-        
-        if self.activity_Pop_thread is not None:
-            self.activity_Pop_thread = None 
+    """Check for local screenshots and upload them to S3."""
+            try:
+                script_dir = os.path.dirname(os.path.abspath(__file__))
+                directory = os.path.join(script_dir, "Queue", "Screen_Shot")
+                
+                if os.path.exists(directory):
+                    files = os.listdir(directory)
+                    for Local in files:
+                        Inet = self.check_internet_connection()
+                        if Inet:
+                            file_path = os.path.join(directory, Local)
+                            if os.path.isfile(file_path):
+                                try:
+                                    # Check if the file is a screenshot (e.g., ends with .png or .jpg)
+                                    if Local.lower().endswith(('.png', '.jpg', '.jpeg')):
+                                        # Open the image file to upload
+                                        with Image.open(file_path) as image:
+                                            # Upload the screenshot to S3
+                                            self.upload_SS_to_s3(image,self.file_name_ss,Local)  # Use the correct upload function
+                                            print(f"Uploaded screenshot {Local} to S3.")
+                                        
+                                        # Delete the file after successful upload
+                                        os.remove(file_path)
+                                        print(f"Deleted local screenshot file: {Local}")
+                                except Exception as e:
+                                    print(f"Error uploading {Local} to S3: {str(e)}")
+            except Exception as e:
+                print(f"Error checking local screenshots for upload: {str(e)}")
