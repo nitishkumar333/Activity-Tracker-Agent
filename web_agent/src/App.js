b6629d30bac9324312a0d5b6692a20c58ed2baca
nitishkumar333 <ae.nitishkumar@gmail.com>
2024-10-03 14:50:43 +0530

    S3 connect with react

--- web_agent/src/App.js
+++ web_agent/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import AWS from 'aws-sdk';
 import './App.css';
 
 function App() {
@@ -8,6 +9,16 @@ function App() {
     toggle: false,
   });
 
+  // AWS S3 Configuration
+  AWS.config.update({
+    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+    region: process.env.REACT_APP_AWS_REGION_NAME,
+  });
+
+  const s3 = new AWS.S3();
+  const BUCKET_NAME = process.env.REACT_APP_AWS_BUCKET_NAME;
+
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
     setFormData({
@@ -15,9 +26,28 @@ function App() {
       [name]: type === 'checkbox' ? checked : value,
     });
   };
+  
+  const uploadToS3 = async (data) => {
+    const fileName = `python_agent_data.json`;
+
+    const params = {
+      Bucket: BUCKET_NAME,
+      Key: fileName,
+      Body: JSON.stringify(data),
+      ContentType: 'application/json',
+    };
+
+    try {
+      await s3.upload(params).promise();
+      alert(`File uploaded successfully`);
+    } catch (error) {
+      alert('Error uploading file to S3');
+    }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    await uploadToS3(formData);
   };
 
   return (
