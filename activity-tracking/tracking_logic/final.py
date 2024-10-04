df9ac397559eb59764eeecc4a172bb8465856500
Adixit8604 <abhimrt8604@gmail.com>
2024-10-05 01:08:21 +1300

    TimeZone PopUP

--- activity-tracking/tracking_logic/final.py
+++ activity-tracking/tracking_logic/final.py
@@ -3,7 +3,7 @@ import psutil
 import time
 from tracking_logic import mouse_tracking, mouse_click_tracking, keyboard_tracking, timezone_tracking
 
-def Final_Tracker(bot_activity_detected, stop):
+def Final_Tracker(bot_activity_detected, stop,Time_pop):
    
     # Start thread for tracking mouse movement
     movement_thread = threading.Thread(target=mouse_tracking.track_mouse_movement, args=(bot_activity_detected,), daemon=True)
@@ -14,7 +14,7 @@ def Final_Tracker(bot_activity_detected, stop):
     keyboard_thread.start()
 
     # Start thread for detecting time zone changes (will not be paused)
-    timezone_thread = threading.Thread(target=timezone_tracking.detect_time_zone_change, args=(stop,), daemon=True)
+    timezone_thread = threading.Thread(target=timezone_tracking.detect_time_zone_change, args=(stop,Time_pop), daemon=True)
     timezone_thread.start()
 
     # Detect mouse clicks in its own thread
